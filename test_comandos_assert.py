f4cee0bc1a0ae479cca2c2a2e58ce2a0c0335ede
Liedson <liedsonkauam01@gmail.com>
2023-11-06 10:22:25 -0300

    subindo alteração do assert

---
+++ test_comandos_assert.py
@@ -0,0 +1,15 @@
+# assert numbers
+# num_esperado = 2
+# num_obtido = 1
+# assert num_esperado != num_obtido, f"Esperado {num_esperado} não é maior que o número atual {num_obtido}"
+
+# assert text
+# text_esperado = "Selenium Webdriver"
+# text_obtido = "Selenium Webdriver"
+# assert text_esperado.lower() == text_obtido.lower(), "O texto esperado é diferente do texto obtido"
+
+# assert text in string 
+# text_esperado = "Seleniumzzzzz"
+# text_obtido = "Selenium Webdriver"
+# assert text_esperado in text_obtido, f"Ësperado {text_esperado}. Atual {text_obtido}"
+# assert text_esperado not in text_obtido, f"Ësperado {text_esperado}. Atual {text_obtido}"\
